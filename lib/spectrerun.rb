19a494d6bb624c0c78a736399200e555c2514e9b
Nick Johnstone <ncwjohnstone@gmail.com>
2015-12-03 20:12:22 +1300

    Make all the things work

---
+++ lib/spectrerun.rb
@@ -0,0 +1,64 @@
+require 'pathname'
+
+def ruby_files
+  @file_names ||= Dir.glob('**/*.rb')
+end
+
+def declared_constants(file_name)
+  [
+    file_name,
+
+    File
+      .readlines(file_name)
+      .select { |line| line.start_with? 'class' }
+      .map(&:chomp)
+      .map { |declaration| declaration.split[1] }
+  ]
+end
+
+def constants_used_in_file(file_name)
+  [
+    file_name,
+
+    File
+      .readlines(file_name)
+      .map { |line| line.match(/.*\W([A-Z]\w+)/) }
+      .compact
+      .map { |match| match[1] }
+  ]
+end
+
+def files_impacted(constants_used_in_files, constant)
+  constants_used_in_files.select do |_, constants|
+    constants.include? constant
+  end
+end
+
+def spec_path(file_name)
+  return file_name if file_name.end_with? '_spec.rb'
+
+  file_name
+    .gsub('app', 'spec')
+    .gsub('.rb', '_spec.rb')
+end
+
+def find_specs_to_run(changed_files)
+  constants_in_files = Hash[
+    ruby_files.map { |filename| declared_constants(filename) }
+  ]
+
+  constants_used_in_files = Hash[
+    ruby_files.map { |file_name| constants_used_in_file(file_name) }
+  ]
+
+  constants_impacted = changed_files
+    .flat_map { |file_name| constants_in_files[file_name] }
+    .map { |constant| constant.split('::').last }
+
+  files_to_check = constants_impacted
+    .flat_map { |constant| files_impacted(constants_used_in_files, constant) }
+    .flat_map(&:keys)
+    .map { |file_name| spec_path(file_name) }
+    .uniq
+    .select { |file_name| File.exist? file_name }
+end
---
+++ spectrerun.gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |s|
+  s.name        = "spectrerun"
+  s.version     = "0.0.1"
+  s.date        = "2015-12-03"
+  s.summary     = "Find all the specs you need to run"
+  s.description = "Given a list of files that have change, find all the specs that should be run"
+  s.authors     = ["Nick Johnstone"]
+  s.email       = 'ncwjohnstone@gmail.com'
+  s.files       = ["lib/spectrerun.rb"]
+  s.executables << "spectrerun"
+  s.homepage    = "https://github.com/Widdershin/spectrerun"
+  s.license     = "MIT"
+end
