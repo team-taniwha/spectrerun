be8297334db7cf2a1e0fbd0cfb289512a8da1022
Nick Johnstone <ncwjohnstone@gmail.com>
2015-12-04 11:20:56 +1300

    Fix analyzing files that don't impact constants, closes #2

--- lib/spectrerun.rb
+++ lib/spectrerun.rb
@@ -53,7 +53,10 @@ def find_specs_to_run(changed_files)
 
   constants_impacted = changed_files
     .flat_map { |file_name| constants_in_files[file_name] }
+    .compact
     .map { |constant| constant.split('::').last }
+
+  return if constants_impacted.empty?
 
   files_to_check = constants_impacted
     .flat_map { |constant| files_impacted(constants_used_in_files, constant) }
